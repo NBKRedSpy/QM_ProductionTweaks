c8581ae10969404f7dc43af615a750d8f7420549
NBKRedSpy <54865934+NBKRedSpy@users.noreply.github.com>
2025-03-17 15:34:30 -0400

    WIP

--- src/ContextSplitProductionButton_Initialize_Patch.cs
+++ src/ContextSplitProductionButton_Initialize_Patch.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace QM_ProductionTweaks
 {
@@ -12,16 +13,47 @@ namespace QM_ProductionTweaks
     /// <summary>
     /// Change the initial value to be the maximum that can be produced instead of 1.
     /// </summary>
-    [HarmonyPatch(typeof(ContextSplitProductionButton), nameof(ContextSplitProductionButton.Initialize))]
+    [HarmonyPatch(typeof(MagnumSelectItemToProduceWindow), nameof(MagnumSelectItemToProduceWindow.ReceiptPanelOnStartProduction))]
     internal static class ContextSplitProductionButton_Initialize_Patch
     {
-        public static void Postfix(ContextSplitProductionButton __instance)
+        public static void Postfix(MagnumSelectItemToProduceWindow __instance, ItemReceiptPanel panel)
         {
-            if(__instance._maxCount > 1)
+
+            //Debug testing
+            __instance._receiptToProduce = panel.Receipt;
+            int maxCraft = ItemProductionSystem.GetAvailableToProduceCount(__instance._magnumCargo.ShipCargo,
+                __instance._receiptToProduce.RequiredItems);
+            int num = 1;
+            CompositeItemRecord compositeItemRecord = Data.Items.GetRecord(__instance._receiptToProduce.OutputItem) as CompositeItemRecord;
+            foreach (BasePickupItemRecord record in compositeItemRecord.Records)
             {
-                __instance.OnValueChanged(__instance._maxCount);
-                __instance._slider.value = __instance._slider.maxValue;
+                if (record is IStackableRecord stackableRecord && compositeItemRecord.PrimaryRecord == record)
+                {
+                    num = stackableRecord.MaxStack;
+                    break;
+                }
             }
+            bool num2 = num > 1;
+            bool flag = maxCraft == int.MaxValue;
+            if (num2)
+            {
+                maxCraft = (flag ? num : Mathf.Min(maxCraft, num));
+            }
+            else
+            {
+                maxCraft = (flag ? 5 : Mathf.Min(maxCraft, 5));
+            }
+            UI.Chain<CommonContextMenu>().Invoke(delegate (CommonContextMenu v)
+            {
+                v.AddSliderCommand(maxCraft, 1, maxCraft);
+            }).Invoke(delegate (CommonContextMenu v)
+            {
+                v.SetupCommand(Localization.Get("ui.context.StartProduction"), 0);
+            })
+                .Show()
+                .SetBackgroundOrder(-1)
+                .SetBackOnBackgroundClick(value: true);
+            UI.Get<CommonContextMenu>().SnapToPosition(panel.transform.GetCenterPosition());
         }
     }
 }
--- src/MagnumSelectItemToProduceWindow_Hide_Patch.cs
+++ src/MagnumSelectItemToProduceWindow_Hide_Patch.cs
@@ -8,12 +8,13 @@ using System.Threading.Tasks;
 
 namespace QM_ProductionTweaks
 {
-    [HarmonyPatch(typeof(MagnumSelectItemToProduceWindow), nameof(MagnumSelectItemToProduceWindow.Hide))]
-    internal static class MagnumSelectItemToProduceWindow_Hide_Patch
-    {
-        public static void Postfix(MagnumSelectItemToProduceWindow __instance)
-        {
-            MagnumSelectItemToProduceWindow_InitPanels_Patch.LastPosition = __instance._scrollRect.normalizedPosition;
-        }
-    }
+    //Debug
+    //[HarmonyPatch(typeof(MagnumSelectItemToProduceWindow), nameof(MagnumSelectItemToProduceWindow.Hide))]
+    //internal static class MagnumSelectItemToProduceWindow_Hide_Patch
+    //{
+    //    public static void Postfix(MagnumSelectItemToProduceWindow __instance)
+    //    {
+    //        MagnumSelectItemToProduceWindow_InitPanels_Patch.LastPosition = __instance._scrollRect.normalizedPosition;
+    //    }
+    //}
 }
