5017243c52ee043a0f53bc474bc793819a5e9cd5
NBKRedSpy <54865934+NBKRedSpy@users.noreply.github.com>
2025-03-18 01:08:30 -0400

    WIP

--- src/MagnumSelectItemToProduceWindow_ReceiptPanelOnStartProduction_Patch.cs
+++ src/MagnumSelectItemToProduceWindow_ReceiptPanelOnStartProduction_Patch.cs
@@ -3,9 +3,13 @@ using MGSC;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using static Rewired.Demos.CustomPlatform.MyPlatformControllerExtension;
 
 namespace QM_ProductionTweaks
 {
@@ -13,51 +17,91 @@ namespace QM_ProductionTweaks
     /// <summary>
     /// Change the initial value to be the maximum that can be produced instead of 1.
     /// </summary>
-    [HarmonyPatch(typeof(MagnumSelectItemToProduceWindow), nameof(MagnumSelectItemToProduceWindow.ReceiptPanelOnStartProduction))]
+    [HarmonyPatch]
     internal static class MagnumSelectItemToProduceWindow_ReceiptPanelOnStartProduction_Patch
     {
-        public static bool Prefix(MagnumSelectItemToProduceWindow __instance, ItemReceiptPanel panel)
+
+        public static IEnumerable<MethodBase> TargetMethods()
         {
+            MethodInfo directMethod = typeof(MagnumSelectItemToProduceWindow)
+                .GetNestedTypes(AccessTools.all)
+                .Single(x => x.Name == "<>c__DisplayClass30_0")
+                .GetMethods(AccessTools.all)
+                .FirstOrDefault(x => x.Name == "<ReceiptPanelOnStartProduction>b__0");
 
-            //Debug testing
-            __instance._receiptToProduce = panel.Receipt;
-            int maxCraft = ItemProductionSystem.GetAvailableToProduceCount(__instance._magnumCargo.ShipCargo,
-                __instance._receiptToProduce.RequiredItems);
-            int num = 1;
-            CompositeItemRecord compositeItemRecord = Data.Items.GetRecord(__instance._receiptToProduce.OutputItem) as CompositeItemRecord;
-            foreach (BasePickupItemRecord record in compositeItemRecord.Records)
-            {
-                if (record is IStackableRecord stackableRecord && compositeItemRecord.PrimaryRecord == record)
-                {
-                    num = stackableRecord.MaxStack;
-                    break;
-                }
-            }
-            bool num2 = num > 1;
-            bool flag = maxCraft == int.MaxValue;
-            if (num2)
-            {
-                maxCraft = (flag ? num : Mathf.Min(maxCraft, num));
-            }
-            else
-            {
-                maxCraft = (flag ? 5 : Mathf.Min(maxCraft, 5));
-            }
-            UI.Chain<CommonContextMenu>().Invoke(delegate (CommonContextMenu v)
-            {
-                v.AddSliderCommand(maxCraft, 1, maxCraft);
-            }).Invoke(delegate (CommonContextMenu v)
-            {
-                v.SetupCommand(Localization.Get("ui.context.StartProduction"), 0);
-            })
-                .Show()
-                .SetBackgroundOrder(-1)
-                .SetBackOnBackgroundClick(value: true);
-            UI.Get<CommonContextMenu>().SnapToPosition(panel.transform.GetCenterPosition());
 
+            yield return directMethod;
+        }
+
+        public static bool Prefix(object __instance, MGSC.CommonContextMenu v, int ___maxCraft)
+        {
+            v.AddSliderCommand(___maxCraft, 1, ___maxCraft);
             return false;
         }
 
-        
+
+        //public static void Prefix(MagnumSelectItemToProduceWindow __instance)
+        //{
+        //    //Set the initial value to the maximum that can be produced.
+
+        //    Type myClassType = typeof(MagnumSelectItemToProduceWindow);
+        //    List<MethodInfo> anonymousMethods = myClassType
+        //        .GetMethods(
+        //              BindingFlags.NonPublic
+        //            | BindingFlags.Public
+        //            | BindingFlags.Instance
+        //            | BindingFlags.Static)
+        //        .Where(method =>
+        //              method.GetCustomAttributes(typeof(CompilerGeneratedAttribute)).Any())
+        //        .ToList();
+        //}
+
+        //public static Enumerable<CodeInstruction> Transpile(Enumerable<CodeInstruction> instructions)
+        //{
+
+        //    // See original IL source here: OriginalILReference / MGSC.SpawnItemCommand.il
+
+        //    //Goal: The spawn duplicates the DatadiskRecord unlock randomization that is already
+        //    //  in the CreateForInventory function.  This would undo the Pity unlock code when using
+        //    //  this spawn command for testing.
+
+        //    List<CodeInstruction> original = instructions.ToList();
+
+        //    //Debugging
+        //    //Utils.LogIL(original);
+
+
+        //    List<CodeInstruction> result = new CodeMatcher(original)
+
+        //        //Goal: replace the type check with a false to just bypass the if block.
+
+        //        //// if (basePickupItem.Is<DatadiskRecord>())
+        //        //IL_000f: ldloc.0
+        //        //IL_0010: callvirt instance bool MGSC.BasePickupItem::Is<class MGSC.DatadiskRecord>()
+
+        //        .MatchStartForward(
+        //            new CodeMatch(OpCodes.Ldloc_0),
+        //            new CodeMatch(OpCodes.Callvirt,
+        //                AccessTools.Method(typeof(BasePickupItem), "Is", new Type[] { }, new Type[] { typeof(DatadiskRecord) })),
+        //            new CodeMatch(OpCodes.Brfalse_S)
+        //        )
+        //        .ThrowIfNotMatch("Did not find 'if (basePickupItem.Is<DatadiskRecord>())'")
+
+        //        .RemoveInstructions(2)
+
+        //        //Push a 0 (false)
+        //        .InsertAndAdvance(
+        //            new CodeInstruction(OpCodes.Ldc_I4_0)
+        //        )
+        //        .InstructionEnumeration()
+        //        .ToList();
+
+        //    //Post update.
+        //    //Utils.LogIL(result);
+
+        //    return result;
+        //}
+
+
     }
 }
