ca52aae851d892e5fa49098a1f2f81628306edaa
NBKRedSpy <54865934+NBKRedSpy@users.noreply.github.com>
2025-03-18 01:40:57 -0400

    WIP

--- src/MagnumSelectItemToProduceWindow_ReceiptPanelOnStartProduction_Patch.cs
+++ src/MagnumSelectItemToProduceWindow_ReceiptPanelOnStartProduction_Patch.cs
@@ -21,87 +21,38 @@ namespace QM_ProductionTweaks
     internal static class MagnumSelectItemToProduceWindow_ReceiptPanelOnStartProduction_Patch
     {
 
+        /// <summary>
+        /// The GUI configure is a lambda, so have to set the target this way.
+        /// Have to find the name from the IL first.
+        /// </summary>
+        /// <returns></returns>
         public static IEnumerable<MethodBase> TargetMethods()
         {
             MethodInfo directMethod = typeof(MagnumSelectItemToProduceWindow)
                 .GetNestedTypes(AccessTools.all)
                 .Single(x => x.Name == "<>c__DisplayClass30_0")
                 .GetMethods(AccessTools.all)
-                .FirstOrDefault(x => x.Name == "<ReceiptPanelOnStartProduction>b__0");
+                .Single(x => x.Name == "<ReceiptPanelOnStartProduction>b__0");
 
 
             yield return directMethod;
         }
 
+
         public static bool Prefix(object __instance, MGSC.CommonContextMenu v, int ___maxCraft)
         {
+            //------- The goal is to change this code to use the max value:
+            //  ...
+            // 	UI.Chain<CommonContextMenu>().Invoke(delegate(CommonContextMenu v)
+            // 	{
+            // 		v.AddSliderCommand(1, 1, maxCraft);
+            // 	}).Invoke(delegate(CommonContextMenu v)
+            // 	{
+            //  ...
+
             v.AddSliderCommand(___maxCraft, 1, ___maxCraft);
             return false;
         }
-
-
-        //public static void Prefix(MagnumSelectItemToProduceWindow __instance)
-        //{
-        //    //Set the initial value to the maximum that can be produced.
-
-        //    Type myClassType = typeof(MagnumSelectItemToProduceWindow);
-        //    List<MethodInfo> anonymousMethods = myClassType
-        //        .GetMethods(
-        //              BindingFlags.NonPublic
-        //            | BindingFlags.Public
-        //            | BindingFlags.Instance
-        //            | BindingFlags.Static)
-        //        .Where(method =>
-        //              method.GetCustomAttributes(typeof(CompilerGeneratedAttribute)).Any())
-        //        .ToList();
-        //}
-
-        //public static Enumerable<CodeInstruction> Transpile(Enumerable<CodeInstruction> instructions)
-        //{
-
-        //    // See original IL source here: OriginalILReference / MGSC.SpawnItemCommand.il
-
-        //    //Goal: The spawn duplicates the DatadiskRecord unlock randomization that is already
-        //    //  in the CreateForInventory function.  This would undo the Pity unlock code when using
-        //    //  this spawn command for testing.
-
-        //    List<CodeInstruction> original = instructions.ToList();
-
-        //    //Debugging
-        //    //Utils.LogIL(original);
-
-
-        //    List<CodeInstruction> result = new CodeMatcher(original)
-
-        //        //Goal: replace the type check with a false to just bypass the if block.
-
-        //        //// if (basePickupItem.Is<DatadiskRecord>())
-        //        //IL_000f: ldloc.0
-        //        //IL_0010: callvirt instance bool MGSC.BasePickupItem::Is<class MGSC.DatadiskRecord>()
-
-        //        .MatchStartForward(
-        //            new CodeMatch(OpCodes.Ldloc_0),
-        //            new CodeMatch(OpCodes.Callvirt,
-        //                AccessTools.Method(typeof(BasePickupItem), "Is", new Type[] { }, new Type[] { typeof(DatadiskRecord) })),
-        //            new CodeMatch(OpCodes.Brfalse_S)
-        //        )
-        //        .ThrowIfNotMatch("Did not find 'if (basePickupItem.Is<DatadiskRecord>())'")
-
-        //        .RemoveInstructions(2)
-
-        //        //Push a 0 (false)
-        //        .InsertAndAdvance(
-        //            new CodeInstruction(OpCodes.Ldc_I4_0)
-        //        )
-        //        .InstructionEnumeration()
-        //        .ToList();
-
-        //    //Post update.
-        //    //Utils.LogIL(result);
-
-        //    return result;
-        //}
-
-
     }
+
 }
